213796d30d02a2cb515bd1353972c8a6a3841323
Favouredd <99230456+Favouredd@users.noreply.github.com>
2022-03-03 01:25:24 -0500

    Update etechApp.groovy

--- vars/etechApp.groovy
+++ vars/etechApp.groovy
@@ -5,7 +5,7 @@ def call(String repoUrl){
 	stages{
 		stage('git-clone'){
 			steps{
-			     checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'git-check', url: 'https://github.com/Favouredd/module2_ci.git']]])	
+			     checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'git-check', url: 'https://github.com/Favouredd/etechApp']]])	
 			}
 		}
 		stage('etech-hello'){
